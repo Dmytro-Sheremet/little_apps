e77cbe7c8a4ec4eb4cd3c6de001b4ce282e217cb
Dmytro-Sheremet <dmytro.sheremet.lviv@gmail.com>
2018-12-07 03:42:31 +0200

    bug fixed

--- largestNumberInArray.js
+++ largestNumberInArray.js
@@ -1,5 +1,5 @@
 let array = [
-    [1990, 10, 07, 'sunday' ],
+    [1990, 10, 07, 'sunday'],
     [11, 12, 13, 14],
     [21, 22, 23, 24],
     [063, 185, 35, 44],
@@ -8,13 +8,18 @@ let array = [
 
 function check(arr) {
     let maxArr = [];
-    for (i = 0; i < arr.length; i++) {
-        for (k = 0; k < arr[i].length; k++) {
+    for (let i = 0; i < arr.length; i++) {
+        // console.log('__________')
+        let startMin = arr[i][0];
+        for (let k = 0; k < arr[i].length; k++) {
             if (typeof (arr[i][k]) === 'number') {
-                if (arr[i][0] < arr[i][k]) {
-                    maxArr[i] = arr[i][k]
+                if (arr[i][k] >= startMin) {
+                    startMin = arr[i][k];
                 }
-            }else{maxArr[i] = 'Not just a Number inside !';}
+                maxArr[i] = startMin;
+            } else {
+                maxArr[i] = 'Not just a Number inside !';
+            }
         }
     }
     return maxArr;
